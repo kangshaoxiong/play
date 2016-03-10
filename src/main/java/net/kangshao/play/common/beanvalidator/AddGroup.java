8e677b7762d0facaa73343b5e37aa5469e67bd06
kangshaoxiong <892885829@qq.com>
2016-03-10 17:11:42 +0800

    调整结构

---
+++ src/main/java/net/kangshao/play/common/beanvalidator/AddGroup.java
@@ -0,0 +1,10 @@
+package net.kangshao.play.common.beanvalidator;
+
+/**
+ * 添加Bean验证组
+ * @author ThinkGem
+ *
+ */
+public interface AddGroup {
+
+}
---
+++ src/main/java/net/kangshao/play/common/beanvalidator/EditGroup.java
@@ -0,0 +1,9 @@
+package net.kangshao.play.common.beanvalidator;
+
+/**
+ * 编辑Bena验证组
+ * @author ThinkGem
+ */
+public interface EditGroup {
+
+}
