d16683b15d6d0d12aff6658fd56daa1f147741a7
kangshaoxiong <892885829@qq.com>
2016-03-10 17:35:15 +0800

    设置默认转入链接

--- src/main/java/net/kangshao/play/modules/sys/controller/SysLoginController.java
+++ src/main/java/net/kangshao/play/modules/sys/controller/SysLoginController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value="/manager/sys")
+@RequestMapping(value="/manager")
 public class SysLoginController {
 	@RequestMapping(value="/login",method=RequestMethod.GET)
 	public String login(){
